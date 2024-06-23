f856c1de61eda3cba70fb1d8a0010358f71b0701
charleysmithschofield <charleysmithschofield@gmail.com>
2024-06-22 17:55:37 -0700

    add methods to filter candidates based on their experience, github points, language knowledge, if they have applied recently, over 17 years old

--- filters.rb
+++ filters.rb
@@ -1,16 +1,48 @@
+# filters.rb
+
 # In this file we define the methods to help filter out candidates
 # This way, we keep these methods separated from other potential parts of the program
+# filters.rb
 
-def find(id)
-    # Your code Here
+# Helper method: Checks if a candidate is experienced (>= 2 years of experience)
+def experienced?(candidate)
+  candidate[:years_of_experience] >= 2
+end
+
+# Helper method: Checks if a candidate has 100 or more GitHub points
+def github_points?(candidate)
+  candidate[:github_points] >= 100
+end
+
+# Helper method: Checks if a candidate knows Ruby or Python
+def knows_ruby_or_python?(candidate)
+  candidate[:languages].include?('Ruby') || candidate[:languages].include?('Python')
+end
+
+# Helper method: Checks if a candidate applied in the last 15 days
+def applied_recently?(candidate)
+  candidate[:date_applied] >= Date.today - 15
+end
+
+# Helper method: Checks if a candidate is over the age of 17
+def over_17?(candidate)
+  candidate[:age] > 17
+end
+
+# Method to filter qualified candidates based on multiple criteria
+def qualified_candidates(candidates)
+  candidates.select do |candidate|
+    experienced?(candidate) &&
+      github_points?(candidate) &&
+      knows_ruby_or_python?(candidate) &&
+      applied_recently?(candidate) &&
+      over_17?(candidate)
   end
-  
-  def experienced?(candidate)
-    # Your code Here
-  end
-  
-  def qualified_candidates(candidates)
-    # Your code Here
-  end
-  
-  # More methods will go below
+end
+
+# Method to sort candidates by qualifications (experience and GitHub points)
+def ordered_by_qualifications(candidates)
+  candidates.sort_by { |candidate| [-candidate[:years_of_experience], -candidate[:github_points]] }
+end
+
+# More methods will go below
--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 # This is the main entrypoint into the program
 # It requires the other files/gems that it needs
+require 'active_support/all'
 require 'pp'
 require './candidates'
 require './filters'
